69dd3d167e218e07a6442f121d8c4358fc4baf7a
Alexandre <alexandreaddres@gmail.com>
2024-02-10 17:54:36 -0300

    return response from tag created

--- src/middleware/tag_creator_view.py
+++ src/middleware/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.middleware.http_types.http_request import HttpRequest
 from src.middleware.http_types.http_response import HttpResponse
+from src.controller.tag_creator_controller import CreateTagController
 
 
 class TagCreatorView:
@@ -7,13 +8,15 @@ class TagCreatorView:
         responsability for interacting with HTTP
     '''
 
+    def __init__(self) -> None:
+        self.create_tag_controller = CreateTagController()
+
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+
         body = http_request.body
+
         product_code = body["product_code"]
-        print(product_code)
 
-        # create tags
+        tag_response = self.create_tag_controller.create(product_code=product_code)
 
-        print(f'On validate and create request with this request: {str(http_request)}')
-        # http return
-        return HttpResponse(status_code=200, body={"right": "all"})
+        return HttpResponse(status_code=201, body=tag_response)
